9d77c31da475e9e291b35f8a5e2b1b719b93616f
C05 <c05@C05s-iMac.local>
2024-06-17 09:17:49 +0900

    homework

---
+++ dags/dags_bash_operator_standard.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.bash import BashOperator
+
+my_dag = DAG(
+    dag_id="dags_bash_operator_standard",
+    schedule="0 9 * * 1,5",
+    start_date=pendulum.datetime(2024, 6, 1, tz="Asia/Seoul"),
+    catchup=False,
+    tags=["homework"]
+)
+
+bash_t1 = BashOperator(
+    task_id="bash_t1",
+    bash_command="echo whoami",
+    dag=my_dag
+    )
+
+bash_t2 = BashOperator(
+    task_id="bash_t2",
+    bash_command="echo $HOSTNAME",
+    dag=my_dag
+    )
+
+bash_t1 >> bash_t2
